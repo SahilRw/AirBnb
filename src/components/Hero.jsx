74b1f3ba4751652d8c9366fc1b0be361abf57808
Sahil R <sahilrw000@gmail.com>
2023-05-06 20:28:33 +0530

    made the navbar bigger and adjusted the text spacing and changed the title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <section className='flex flex-col'>
         <img src="photo-grid.png" alt="grid" className="w-96 max-w-full self-center" />
-            <h1 className='my-2.5 text-4xl font-bold self-center'>Online Experiences</h1>
-            <p className='mt-0 self-center'>Join unique interactive activities led by 
+            <h1 className='my-6 text-4xl font-bold self-center'>Online Experiences</h1>
+            <p className='-mt-2 self-center max-w-sm text-center'>Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
     </section>
   )
